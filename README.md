6fcc1dd4ce49af3f562a7e81955b5e0f142f475f
João Guilherme <168691691+JoaoGChv@users.noreply.github.com>
2025-06-28 03:01:26 -0300

    Allow custom dataset path

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # miss_annotation
+
+Ferramenta simples para anotar imagens usando uma interface Tkinter.
+
+## Uso
+
+1. Construa a imagem Docker:
+
+```bash
+./build.sh
+```
+
+2. Execute o contêiner apontando para o diretório do seu dataset:
+
+```bash
+DATASET_PATH=/home/joao/Downloads/train ./run.sh
+```
+
+Em sistemas Windows utilizando WSL, utilize `run_wsl.sh` no lugar de `run.sh`.
+
+O diretório indicado em `DATASET_PATH` deve conter pelo menos as pastas
+`images/` e o arquivo `annotations.yaml`. Caso exista a pasta opcional
+`Segmentations/`, ela será ignorada pela aplicação.
+
+O dataset será montado dentro do contêiner em `/app/dataset`.
--- run_wsl.sh
+++ run_wsl.sh
@@ -3,6 +3,10 @@ set -e
 
 echo "Iniciando o container pytorch-tkinter-app com suporte a GUI"
 
+# Caminho para o dataset no host. Defina a variável DATASET_PATH para usar um
+# diretório diferente. Se não definido, assume 'dataset' dentro do repo.
+DATASET_PATH=${DATASET_PATH:-"$(pwd)/dataset"}
+
 # Detecta IP do servidor X no host Windows
 HOST_IP=$(grep nameserver /etc/resolv.conf | awk '{print $2}')
 export DISPLAY=${HOST_IP}:0
@@ -12,7 +16,7 @@ docker run \
   -e DISPLAY=${DISPLAY} \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v "$(pwd)/app.py":/app/app.py:ro \
-  -v "$(pwd)/dataset":/app/dataset:ro \
+  -v "${DATASET_PATH}":/app/dataset:ro \
   -v "$(pwd)/gui":/app/gui:ro \
   -v "$(pwd)/utils":/app/utils:ro \
   -w /app \
